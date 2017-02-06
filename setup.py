6ef8e3150a68da06e87c2a732d62a65eb58de426
Alex Fu <alex.fu914@gmail.com>
2017-02-06 12:17:06 -0500

    Update version.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='changelogger',
-    version='0.0.1',
+    version='0.1.0',
     py_modules=['changelogger'],
     install_requires=[
         'Click'
